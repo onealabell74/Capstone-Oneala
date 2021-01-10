24df365d4a6623f255932907049cdbd3773ab89e
Oneala Bell <73319883+onealabell74@users.noreply.github.com>
2021-01-10 14:35:08 -0600

    updating capstone

--- components/Nav.js
+++ components/Nav.js
@@ -2,25 +2,25 @@ export default () => `
 <nav id="navbar">
     <i class="fas fa-bars"></i>
       <ul class="hidden--mobilenav-links">
-        <li><a href="#">Home</a></li>
-        <li><a href="./About.html">About</a></li>
-        <li><a href="./Contact.html">Contact</a></li>
+        <li><a href="/" data-navigo>Home</a></li>
+        <li><a href="./About.html" data-navigo>About</a></li>
+        <li><a href="./Contact.html" data-navigo>Contact</a></li>
         <li class="dropdown"><a href="javascript:void(0)" class="dropbtn">Types of Dialysis Treatments</a>
           <div class="dropdown-content">
-            <a href="./CenterAndHome.html"> Center/Home</a>
-            <a href="./Peritoneal.html">Peritoneal Dialysis</a>
-            <a href="./ContinousRenalReplacement.html">Continuous Renal Replacement</a>
+            <a href="./CenterAndHome.html" data-navigo> Center/Home</a>
+            <a href="./Peritoneal.html" data-navigo>Peritoneal Dialysis</a>
+            <a href="./ContinousRenalReplacement.html" data-navigo>Continuous Renal Replacement</a>
           </div>
         </li>
         <li class="dropdown"><a href="javascript:void(0)" class="dropbtn">Types of Dialysis Access</a>
           <div class="dropdown-content">
-            <a href="./CentralVenousCatheter.html">Central Venous Catheter(CVC)</a>
-            <a href="./avf.html">Arteriovenous Fistula(AVF)</a>
-            <a href="./ArteriovenousGraft.html">Arteriovenous Graft(AVG)</a>
-            <a href="./PeritonealDialysisCatheter.html">Peritoneal Dialysis Catheter(PD)</a>
+            <a href="./CentralVenousCatheter.html" data-navigo>Central Venous Catheter(CVC)</a>
+            <a href="./avf.html" data-navigo>Arteriovenous Fistula(AVF)</a>
+            <a href="./ArteriovenousGraft.html" data-navigo>Arteriovenous Graft(AVG)</a>
+            <a href="./PeritonealDialysisCatheter.html" data-navigo>Peritoneal Dialysis Catheter(PD)</a>
           </div>
         </li>
-        <li><a href="./Careteam.html">Care Team</a></li>
+        <li><a href="./Careteam.html" data-navigo>Care Team</a></li>
       </ul>
 </nav>
 `;
--- components/views/Centerandhome.js
+++ components/views/Centerandhome.js
@@ -1,6 +1,4 @@
 export default () => `
-<>
-
             <main>
             <h1>Center</h1>
             <p>Center hemodialysis is when a person goes to a dialysis center for their hemodialysis treatments. Hemodialysis is a treatment that filters the blood of wastes and extra fluid when the kidneys are no longer able to perform this function.</p>
