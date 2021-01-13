fcf9cbcc7f306b40736ea97e444749a4d0aa75f0
Oneala Bell <73319883+onealabell74@users.noreply.github.com>
2021-01-13 11:35:15 -0600

    "changes to home"

--- components/views/Home.js
+++ components/views/Home.js
@@ -2,12 +2,6 @@ export default () => `
 <main>
     <p>Dialysis is a treatment for kidney failure that rids your body of unwanted toxins, waste products and excess fluids by filtering your blood. When kidneys fail, your body may have difficulty cleaning your blood and keeping your system chemically balanced. Dialysis can take the place of some kidney function and, along with medication and proper care, help people live longer.
     </p>
-<p>
-    There are many different types of dialysis treatments. You have to decide what
-    best for you. Take in factors your lifestyle, Because dialysis is like
-    having a part time job. Think of at such. I hope this will help you
-    decide what is best for you.
-</p>
 
 <img src="https://thumbs.dreamstime.com/b/superhero-doctor-nurse-medical-masks-hero-cape-staff-protective-suits-188518924.jpg">
 
