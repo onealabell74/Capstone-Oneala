a69542329189e4bb7eeda50fd2929b543ef49bc0
Oneala Bell <73319883+onealabell74@users.noreply.github.com>
2021-01-14 12:51:56 -0600

    "final spa"

--- components/views/Home.js
+++ components/views/Home.js
@@ -1,8 +1,10 @@
-export default () => `
+export default st => `
 <main>
     <p>Dialysis is a treatment for kidney failure that rids your body of unwanted toxins, waste products and excess fluids by filtering your blood. When kidneys fail, your body may have difficulty cleaning your blood and keeping your system chemically balanced. Dialysis can take the place of some kidney function and, along with medication and proper care, help people live longer.
     </p>
 <img id="superhero" src="https://thumbs.dreamstime.com/b/superhero-doctor-nurse-medical-masks-hero-cape-staff-protective-suits-188518924.jpg">
-
+<div>
+        Todays weather in ${st.city} is ${st.description}, and the temperature is ${st.temp} degrees.
+    </div>
 </main>
 `;
--- index.js
+++ index.js
@@ -7,6 +7,30 @@ import * as state from "./store";
 
 const router = new Navigo("/");
 
+router.hooks({
+  before: (done, params) => {
+    // Because not all routes pass params we have to guard against is being undefined
+    const page =
+      params && Object.prototype.hasOwnProperty.call(params, "page")
+        ? capitalize(params.page)
+        : "Home";
+
+    axios
+      .get(
+        "http://api.openweathermap.org/data/2.5/weather?q=st. louis,us&appid=7ab91a28fa72ea7d19c90469792c4526"
+      )
+      .then(response => {
+        const ourdata = response.data;
+        state.Home.city = ourdata.name;
+        state.Home.temp = Math.floor(
+          ((ourdata.main.temp - 273.15) * 9) / 5 + 32
+        );
+        state.Home.description = ourdata.weather[0].description;
+        done();
+      });
+  }
+});
+
 router
   .on({
     "/": () => render(state.Home),
@@ -28,20 +52,3 @@ function render(st = state.Home) {
   `;
   router.updatePageLinks();
 }
-
-axios
-  .get(
-    `http://api.openweathermap.org/data/2.5/weather?q=st.louis,us&appid=7ab91a28fa72ea7d19c90469792c4526`
-  )
-  .then(response => {
-    state.Home.weather.city = response.name;
-    state.Home.weather.temp = response.main.temp;
-    state.Home.weather.description = response.weather.main;
-    console.log(response);
-  });
-
-let city = ourdata.name;
-let temp = ourdata.main.temp;
-let description = ourdata.weather[o].description;
-
-console.log(city, temp, description);
